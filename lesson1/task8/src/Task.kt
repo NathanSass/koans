2233d10ef7488338266992cbdeacca40d97ebe18
nathan <nathans@scribd.com>
2017-06-15 15:42:09 -0700

    new things

--- lesson1/task8/src/Task.kt
+++ lesson1/task8/src/Task.kt
@@ -1,7 +1,12 @@
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ){
-    TODO()
+    val personalInfo: PersonalInfo? =  client?.personalInfo
+
+    val email: String = personalInfo?.email ?: return null
+
+//    if (personalInfo == null || email == null) return null
+    mailer.sendMessage(email, message)
 }
 
 class Client (val personalInfo: PersonalInfo?)
