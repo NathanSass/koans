68b6cff6ce9e598d812e825c3371e840ea589d75
nathan <nathans@scribd.com>
2017-06-22 16:48:07 -0700

    new things

--- lesson2/task4/src/Task.kt
+++ lesson2/task4/src/Task.kt
@@ -1,4 +1,21 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return DateIterator(this)
+    }
+}
+
+class DateIterator(val dataRange:DateRange): Iterator<MyDate> {
+    var current: MyDate = dataRange.start
+    override fun hasNext(): Boolean {
+       return current <= dataRange.end
+    }
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.nextDay()
+        return result
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
